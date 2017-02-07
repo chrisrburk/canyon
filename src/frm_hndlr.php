10a005e075c87e256fc83e0f577797897490d0c7
Cburk <hello@cburk.com>
2017-02-06 20:18:42 -0700

    added more messaging to the form email

--- src/frm_hndlr.php
+++ src/frm_hndlr.php
@@ -17,23 +17,25 @@
         $recipient = "me@chrisbob.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "I want on your list for Canyon Media!";
 
         // Build the email content.
+        $email_content .= "Hello,\n\nThe following email has been received from Canyon.Media\n\n";
         $email_content .= "Email: $email\n\n";
+        $email_content .= "";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "From: <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Thank You! Your message has been sent. Some one from our team will contact you shortly.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! Something went wrong and we couldn't send your message. Please contact us at 719.600.7424";
         }
 
     } else {
